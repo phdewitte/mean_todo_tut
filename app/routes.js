1280ba56b4f6d42efd4e5b34c600db01f7c6524c
Patrick DeWitte <patrickhdewitte@gmail.com>
2016-09-28 15:06:00 -0500

    Work on angular routes

--- app/routes.js
+++ app/routes.js
@@ -1,44 +1,46 @@
 let Todo = require('./models/todo')
 
-app.get('/api/todos', function(request, response) {
-  Todo.find(function(err, todos) {
-    if (err)
-      response.send(err)
-
-    response.json(todos);
-  });
-});
-
-app.post('/api/todos', function(request, response) {
-  Todo.create({
-    text : request.body.text,
-    done : false
-  }, function(err, todo){
-    if (err)
-      response.send(err);
-
+// module.exports = function(app) {
+  app.get('/api/todos', function(request, response) {
     Todo.find(function(err, todos) {
       if (err)
         response.send(err)
-      repsonse.json(todos);
-    });
-  });
-});
 
-app.delete('/api/todos', function(request, response) {
-  Todo.remove({
-    _id : request.params.todo_id
-  }, function(err, todo) {
-    if (err)
-      response.send(err)
-
-    Todo.find(function(err, todos) {
-      if (err)
-        response.send(err)
       response.json(todos);
     });
   });
-});
+
+  app.post('/api/todos', function(request, response) {
+    Todo.create({
+      text : request.body.text,
+      done : false
+    }, function(err, todo){
+      if (err)
+        response.send(err);
+
+      Todo.find(function(err, todos) {
+        if (err)
+          response.send(err)
+        repsonse.json(todos);
+      });
+    });
+  });
+
+  app.delete('/api/todos', function(request, response) {
+    Todo.remove({
+      _id : request.params.todo_id
+    }, function(err, todo) {
+      if (err)
+        response.send(err)
+
+      Todo.find(function(err, todos) {
+        if (err)
+          response.send(err)
+        response.json(todos);
+      });
+    });
+  });
+// }
 
 app.get('*', function(request, response) {
   response.sendfile('./public/index.html');
