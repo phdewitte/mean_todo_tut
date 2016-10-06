ab985f8d9850e2dbc2cb552803389d1e421a4a85
Patrick DeWitte <patrickhdewitte@gmail.com>
2016-10-06 15:24:08 -0500

    Move core angular functions to services and controllers

---
+++ public/js/services/todos.js
@@ -0,0 +1,14 @@
+angular.module('todoService', [])
+  .factory('Todos', ['$http', function($http){
+    return {
+      get : function() {
+        return $http.get('/api/todos');
+      },
+      create : function (todoData) {
+        return $http.post('/api/todos', todoData);
+      },
+      delete : function(id) {
+        return $http.delete('/api/todos/' + id);
+      }
+    }
+  }])
